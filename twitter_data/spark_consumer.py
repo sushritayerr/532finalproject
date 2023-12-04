197c146a4bb0fd53e8fea607033ac04d18c073d0
Sushrita Yerra <sushritayerr@umass.edu>
2023-12-03 20:24:11 -0500

    Updated spark consumer

--- twitter_data/spark_consumer.py
+++ twitter_data/spark_consumer.py
@@ -1,5 +1,11 @@
 from pyspark.sql import SparkSession
+import sys
 import os
+from pyspark.accumulators import AccumulatorParam
+import time
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
 import json
 from transformers import pipeline
 
@@ -8,24 +14,35 @@ output_data = {"google": {"pos": 0, "neg": 0, "neutral": 0}, "verizon": {"pos": 
                "microsoft": {"pos": 0, "neg": 0, "neutral": 0}, "nvidia": {"pos": 0, "neg": 0, "neutral": 0},
                "facebook": {"pos": 0, "neg": 0, "neutral": 0}}
 
+class SentimentAccumulatorParam(AccumulatorParam):
+    def zero(self, initialValue):
+        return {"pos": 0, "neg": 0, "neutral": 0}
 
-def subscribe_event():
-    # Initialize a Spark session
-    spark = SparkSession.builder \
-        .config("spark.jars", os.getcwd() + "/jars/spark-sql-kafka-0-10_2.12-3.5.0.jar" + "," +
-                os.getcwd() + "/jars/kafka-clients-3.4.1.jar" + "," +
-                os.getcwd() + "/jars/commons-pool2-2.11.1.jar" + "," +
-                os.getcwd() + "/jars/spark-token-provider-kafka-0-10_2.12-3.5.0.jar") \
+    def addInPlace(self, v1, v2):
+        v1["pos"] += v2["pos"]
+        v1["neg"] += v2["neg"]
+        v1["neutral"] += v2["neutral"]
+        return v1
+    # Define the Kafka parameters
+kafka_params = {
+    "kafka.bootstrap.servers": "localhost:9092",  # Kafka broker(s)
+    "subscribe": "Google1",  # Kafka topic to subscribe to
+    "startingOffsets": "earliest"  # Start reading from the beginning of the topic
+}
+
+spark = SparkSession.builder \
+        .config("spark.jars", os.getcwd() + "/twitter_data/jars/spark-sql-kafka-0-10_2.12-3.5.0.jar" + "," +
+                os.getcwd() + "/twitter_data/jars/kafka-clients-3.4.1.jar" + "," +
+                os.getcwd() + "/twitter_data/jars/commons-pool2-2.11.1.jar" + "," +
+                os.getcwd() + "/twitter_data/jars/spark-token-provider-kafka-0-10_2.12-3.5.0.jar") \
         .appName("TwitterSentimentAnalysis") \
         .getOrCreate()
 
-    # Define the Kafka parameters
-    kafka_params = {
-        "kafka.bootstrap.servers": "localhost:9092",  # Kafka broker(s)
-        "subscribe": "Google, Verizon, Microsoft, Nvidia, Facebook",  # Kafka topic to subscribe to
-        "startingOffsets": "earliest"  # Start reading from the beginning of the topic
-    }
+sentiment_accumulator = spark.sparkContext.accumulator({"pos": 0, "neg": 0, "neutral": 0}, SentimentAccumulatorParam())
 
+def subscribe_event():
+    # Initialize a Spark session
+    
     # Read data from Kafka
     df = spark.readStream \
         .format("kafka") \
@@ -40,7 +57,7 @@ def subscribe_event():
         .start()
 
     # Start the Spark streaming query
-    query.awaitTermination()
+    query.awaitTermination(timeout=60)
     query.stop()
 
 
@@ -53,15 +70,24 @@ def process_row(row):
     sentiment_analysis = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment-latest")
     print(tweet_content)
     result = sentiment_analysis(tweet_content)[0]['label']
-    update_dict(company_name, result)
+    update_accumulator(company_name, result)
 
 
-def update_dict(company_name, result):
-    company = output_data[company_name.lower()]
-    if result == "positive":
-        company["pos"] = company["pos"] + 1
-    elif result == "negative":
-        company["neg"] = company["neg"] + 1
-    if result == "neutral":
-        company["neutral"] = company["neutral"] + 1
-    print(output_data)
+def update_accumulator(company_name, result):
+    global sentiment_accumulator
+
+    # Update the accumulator
+    sentiment_accumulator.add({"pos": 1 if result == "positive" else 0,
+                               "neg": 1 if result == "negative" else 0,
+                               "neutral": 1 if result == "neutral" else 0})
+
+    # Print the updated accumulator
+    # print(sentiment_accumulator.value)
+start_time = time.time()
+subscribe_event()
+end_time = time.time()
+duration = end_time - start_time
+final_value = sentiment_accumulator.value
+print("Final Sentiment Analysis Result:", final_value)
+print('Duration is ', duration)
+spark.stop()
